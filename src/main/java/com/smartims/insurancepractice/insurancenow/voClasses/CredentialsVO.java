3fc77a217e2a92d28f6116c373c56ab0679b9f6e
SreedharGovind <140377328+SreedharGovind@users.noreply.github.com>
2023-09-26 14:56:57 +0530

    Credentials VO class Added

---
+++ src/main/java/com/smartims/insurancepractice/insurancenow/voClasses/CredentialsVO.java
@@ -0,0 +1,22 @@
+package com.smartims.insurancepractice.insurancenow.voClasses;
+
+public class CredentialsVO {
+    String userName;
+    String  Password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
