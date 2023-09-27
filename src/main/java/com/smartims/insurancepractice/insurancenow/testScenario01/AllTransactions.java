434645648c4d4f439784a62356722dabc25dce5a
Malgabalakrishna <Balakrishna.malga@smartims.com>
2023-09-27 14:47:24 +0530

    Merge branch 'feature/BalaKrishna' of https://github.com/SreedharGovind/InsuranceNowAutomation into feature/BalaKrishna
    # Conflicts:
    #	src/main/java/INOW Automation Data Sheet.xlsx
    #	src/main/java/com/smartims/insurancepractice/insurancenow/testScenario01/ExcelClass.java
    #	src/main/java/com/smartims/insurancepractice/insurancenow/testScenario01/NewBusiness.java

---
+++ src/main/java/com/smartims/insurancepractice/insurancenow/testScenario01/AllTransactions.java
@@ -0,0 +1,17 @@
+package com.smartims.insurancepractice.insurancenow.testScenario01;
+
+
+import com.smartims.insurancepractice.insurancenow.testscenario01.NewBusiness_01;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class AllTransactions {
+    public static void main(String[] args) throws Exception {
+        ChromeDriver driver=CommonClass.getchromedriver();
+
+        NewBusiness_01 nb = new NewBusiness_01();
+        nb.getNewBussines(driver);
+    }
+
+}
