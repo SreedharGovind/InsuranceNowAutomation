49105ec5657db94db9dcf378817e6d6becb72324
SreedharGovind <140377328+SreedharGovind@users.noreply.github.com>
2023-10-04 14:23:34 +0530

    Scenario Values Added

--- src/main/java/com/smartims/insurancepractice/insurancenow/testscenario03/testcase05/CancellationNotice_05.java
+++ src/main/java/com/smartims/insurancepractice/insurancenow/testscenario03/testcase05/CancellationNotice_05.java
@@ -1,59 +1,59 @@
 package com.smartims.insurancepractice.insurancenow.testscenario03.testcase05;
 
-import com.smartims.insurancepractice.insurancenow.commonClasses.CommonClass;
 import com.smartims.insurancepractice.insurancenow.testscenario03.Constants_03;
+import com.smartims.insurancepractice.insurancenow.testscenario03.ExcelUtils_03;
+import com.smartims.insurancepractice.insurancenow.voClasses.CancellationNoticeVO;
 import com.smartims.insurancepractice.insurancenow.voClasses.CredentialsVO;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
-
-
+import java.io.IOException;
 import java.time.Duration;
-
 public class CancellationNotice_05 {
-
-    public void cancellationNotice(ChromeDriver driver) {
-
+    public void getcancellationNotice(ChromeDriver driver) throws IOException {
         Actions actions = new Actions(driver);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
         driver.manage().window().maximize();
+        driver.get(Constants_03.localHostUrl);
         CredentialsVO cvo = new CredentialsVO();
+        CancellationNoticeVO cno = new CancellationNoticeVO();
+
+        cno.setCancellationNoticeNoticeDate(ExcelUtils_03.getCellValueByLabel("cancellationNoticeNoticeDate"));
+        cno.setCancellationNoticeCancellationType(ExcelUtils_03.getCellValueByLabel("cancellationNoticeCancellationType"));
+        cno.setCancellationNoticeAdditionalNoticeText(ExcelUtils_03.getCellValueByLabel("cancellationNoticeAdditionalNoticeText"));
+        cno.setCancellationNoticeReason(ExcelUtils_03.getCellValueByLabel("cancellationNoticeReason"));
+        cno.setCancellationNoticeStartTransaction(ExcelUtils_03.getCellValueByLabel("cancellationNoticeStartTransaction"));
+        cno.setCancellationNoticeEffectiveDate(ExcelUtils_03.getCellValueByLabel("cancellationNoticeEffectiveDate"));
+        cno.setCancellationNoticeDescription(ExcelUtils_03.getCellValueByLabel("cancellationNoticeDescription"));
 
         WebElement policyTab = driver.findElement(By.xpath(Constants_03.policySearchTab));
-
         actions.moveToElement(policyTab).perform();
-
         policyTab.click();
-
-        driver.findElement(By.xpath(Constants_03.policyNumberTextField)).sendKeys("PA0000024-01");
-
+        driver.findElement(By.xpath(Constants_03.policyNumberTextField)).sendKeys("PA0000010-01");
         WebElement searchButton = driver.findElement(By.xpath(Constants_03.searchButton));
-
         actions.moveToElement(searchButton).perform();
-
         searchButton.click();
-
         driver.findElement(By.xpath(Constants_03.startTransactionButton)).click();
-
         Select transaction = new Select(driver.findElement(By.xpath(Constants_03.startNewTransaction)));
-
         transaction.selectByValue("Cancellation Notice");
 //        driver.findElement(By.xpath(Constants_03.startTransactionButton)).click();
 //        Select transaction = new Select(driver.findElement(By.xpath(Constants_03.startNewTransaction)));
 //        transaction.selectByValue("Cancellation");
         driver.findElement(By.xpath(Constants_03.selectButton)).click();
-        driver.findElement(By.xpath(Constants_03.cancellationNoticeDate)).sendKeys("09/23/2023");
+        driver.findElement(By.xpath(Constants_03.cancellationNoticeDate)).sendKeys(cno.getCancellationNoticeNoticeDate(), Keys.TAB);
         driver.findElement(By.xpath(Constants_03.cancellationType)).click();
         Select cancellationType = new Select(driver.findElement(By.xpath(Constants_03.cancellationType)));
         cancellationType.selectByValue("Insured");
-        driver.findElement(By.xpath(Constants_03.additionalNoticeText)).sendKeys("Cancellation Notice Transaction");
+        driver.findElement(By.xpath(Constants_03.additionalNoticeText)).sendKeys(cno.getCancellationNoticeAdditionalNoticeText(), Keys.TAB);
         driver.findElement(By.xpath(Constants_03.startButton)).click();
         driver.findElement(By.xpath(Constants_03.processButton)).click();
     }
 }
+
+
 
 
 
--- src/main/java/com/smartims/insurancepractice/insurancenow/testscenario03/testcase06/Payment_06.java
+++ src/main/java/com/smartims/insurancepractice/insurancenow/testscenario03/testcase06/Payment_06.java
@@ -1,17 +1,49 @@
 package com.smartims.insurancepractice.insurancenow.testscenario03.testcase06;
 
 import com.smartims.insurancepractice.insurancenow.testscenario03.Constants_03;
+
+import com.smartims.insurancepractice.insurancenow.testscenario03.ExcelUtils_03;
+
+import com.smartims.insurancepractice.insurancenow.voClasses.PaymentVO;
+
 import org.openqa.selenium.By;
+
+import org.openqa.selenium.Keys;
+
 import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.interactions.Actions;
+
+
+import java.io.IOException;
 
 import java.time.Duration;
 
+
 public class Payment_06 {
-    public void testCase_06(ChromeDriver driver) throws InterruptedException {
+
+    public void testCase_06(ChromeDriver driver) throws InterruptedException, IOException {
+
         Actions actions = new Actions(driver);
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        PaymentVO pvo = new PaymentVO();
+
+        pvo.setPaymentScreenPolicyNumber(ExcelUtils_03.getCellValueByLabel("paymentScreenPolicyNumber"));
+        pvo.setPaymentScreenPremiumAmount(ExcelUtils_03.getCellValueByLabel("paymentScreenPremiumAmount"));
+
+        pvo.setPaymentScreenBankAccountEntryType(ExcelUtils_03.getCellValueByLabel("paymentScreenBankAccountEntryType"));
+
+        pvo.setPaymentScreenBankAccountType(ExcelUtils_03.getCellValueByLabel("paymentScreenBankAccountType"));
+
+        pvo.setPaymentScreenAccountNumber(ExcelUtils_03.getCellValueByLabel("paymentScreenAccountNumber"));
+
+        pvo.setPaymentScreenRoutingNumber(ExcelUtils_03.getCellValueByLabel("paymentScreenRoutingNumber"));
+
+        pvo.setPaymentScreenName(ExcelUtils_03.getCellValueByLabel("paymentScreenName"));
 
         WebElement policyTab = driver.findElement(By.xpath(Constants_03.policySearchTab));
 
@@ -19,23 +51,36 @@ public class Payment_06 {
 
         policyTab.click();
 
-        driver.findElement(By.xpath(Constants_03.policyNumberTextField)).sendKeys("PA0000025-01");
+        driver.findElement(By.xpath(Constants_03.policyNumberTextField)).sendKeys(pvo.getPaymentScreenPolicyNumber());
 
         WebElement searchButton = driver.findElement(By.xpath(Constants_03.searchButton));
 
         actions.moveToElement(searchButton).perform();
 
         searchButton.click();
+
         driver.findElement(By.xpath(Constants_03.makePayment)).click();
+
         driver.findElement(By.id(Constants_03.pay)).click();
+
         driver.findElement(By.id(Constants_03.newACH)).click();
-        driver.findElement(By.id(Constants_03.newPremiumAmount)).sendKeys("$229.15");
-        driver.findElement(By.id(Constants_03.bankAccountEntryType)).sendKeys("Personal");
-        driver.findElement(By.id(Constants_03.bankAccountType)).sendKeys("Savings");
-        driver.findElement(By.id(Constants_03.accountNumber)).sendKeys("1623069645");
-        driver.findElement(By.id(Constants_03.routingNumber)).sendKeys("011000015");
-        driver.findElement(By.id(Constants_03.achName)).sendKeys("raj");
+
+        driver.findElement(By.id(Constants_03.newPremiumAmount)).sendKeys(pvo.getPaymentScreenPremiumAmount(), Keys.TAB);
+
+        driver.findElement(By.id(Constants_03.bankAccountEntryType)).sendKeys(pvo.getPaymentScreenBankAccountEntryType(), Keys.TAB);
+
+        driver.findElement(By.id(Constants_03.bankAccountType)).sendKeys(pvo.getPaymentScreenBankAccountType());
+
+        driver.findElement(By.id(Constants_03.accountNumber)).sendKeys(pvo.getPaymentScreenAccountNumber());
+
+        driver.findElement(By.id(Constants_03.routingNumber)).sendKeys(pvo.getPaymentScreenRoutingNumber());
+
+        driver.findElement(By.id(Constants_03.achName)).sendKeys(pvo.getPaymentScreenName());
+
         driver.findElement(By.xpath(Constants_03.submitPaymentButton)).click();
+
         driver.findElement(By.id(Constants_03.submitPaymentOk)).click();
+
     }
+
 }
