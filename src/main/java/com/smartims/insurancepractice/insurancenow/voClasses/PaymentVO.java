e15c337f66703ef3ca4b3c89150b4a30d2d44ce6
SreedharGovind <140377328+SreedharGovind@users.noreply.github.com>
2023-10-04 12:36:23 +0530

    Payment VO Class

---
+++ src/main/java/com/smartims/insurancepractice/insurancenow/voClasses/PaymentVO.java
@@ -0,0 +1,95 @@
+package com.smartims.insurancepractice.insurancenow.voClasses;
+
+public class PaymentVO {
+   String  paymentScreenPolicyNumber;
+    String paymentScreenNewACH;
+    String    paymentScreenPremiumAmount;
+    String         paymentScreenTotalAmount;
+    String  paymentScreenAreyouchargingadditionalfees;
+
+    String     paymentScreenBankAccountEntryType;
+    String  paymentScreenBankAccountType;
+    String        paymentScreenAccountNumber;
+    String  paymentScreenRoutingNumber;
+    String      paymentScreenName;
+
+    public String getPaymentScreenPolicyNumber() {
+        return paymentScreenPolicyNumber;
+    }
+
+    public void setPaymentScreenPolicyNumber(String paymentScreenPolicyNumber) {
+        this.paymentScreenPolicyNumber = paymentScreenPolicyNumber;
+    }
+
+    public String getPaymentScreenNewACH() {
+        return paymentScreenNewACH;
+    }
+
+    public void setPaymentScreenNewACH(String paymentScreenNewACH) {
+        this.paymentScreenNewACH = paymentScreenNewACH;
+    }
+
+    public String getPaymentScreenPremiumAmount() {
+        return paymentScreenPremiumAmount;
+    }
+
+    public void setPaymentScreenPremiumAmount(String paymentScreenPremiumAmount) {
+        this.paymentScreenPremiumAmount = paymentScreenPremiumAmount;
+    }
+
+    public String getPaymentScreenTotalAmount() {
+        return paymentScreenTotalAmount;
+    }
+
+    public void setPaymentScreenTotalAmount(String paymentScreenTotalAmount) {
+        this.paymentScreenTotalAmount = paymentScreenTotalAmount;
+    }
+
+    public String getPaymentScreenAreyouchargingadditionalfees() {
+        return paymentScreenAreyouchargingadditionalfees;
+    }
+
+    public void setPaymentScreenAreyouchargingadditionalfees(String paymentScreenAreyouchargingadditionalfees) {
+        this.paymentScreenAreyouchargingadditionalfees = paymentScreenAreyouchargingadditionalfees;
+    }
+
+    public String getPaymentScreenBankAccountEntryType() {
+        return paymentScreenBankAccountEntryType;
+    }
+
+    public void setPaymentScreenBankAccountEntryType(String paymentScreenBankAccountEntryType) {
+        this.paymentScreenBankAccountEntryType = paymentScreenBankAccountEntryType;
+    }
+
+    public String getPaymentScreenBankAccountType() {
+        return paymentScreenBankAccountType;
+    }
+
+    public void setPaymentScreenBankAccountType(String paymentScreenBankAccountType) {
+        this.paymentScreenBankAccountType = paymentScreenBankAccountType;
+    }
+
+    public String getPaymentScreenAccountNumber() {
+        return paymentScreenAccountNumber;
+    }
+
+    public void setPaymentScreenAccountNumber(String paymentScreenAccountNumber) {
+        this.paymentScreenAccountNumber = paymentScreenAccountNumber;
+    }
+
+    public String getPaymentScreenRoutingNumber() {
+        return paymentScreenRoutingNumber;
+    }
+
+    public void setPaymentScreenRoutingNumber(String paymentScreenRoutingNumber) {
+        this.paymentScreenRoutingNumber = paymentScreenRoutingNumber;
+    }
+
+    public String getPaymentScreenName() {
+        return paymentScreenName;
+    }
+
+    public void setPaymentScreenName(String paymentScreenName) {
+        this.paymentScreenName = paymentScreenName;
+    }
+}
